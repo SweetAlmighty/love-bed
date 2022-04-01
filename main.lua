4432ffdcf8ea822208548514baff48bbca859269
Brian Sweet <bsweet48@gmail.com>
2022-03-31 23:09:24 -0400

    Moved libs outside of src. First pass at player, world, component, and system files and tests. Yay tdd.

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
 --- summary.
 -- Main.lua.
 -- The entry point for the application.
-
-function love.load() print("Hello") end
---
+++ src/ecs/systems/player_control_system.lua
@@ -0,0 +1,11 @@
+local Concord = require("libs/concord")
+local PositionComponent = require('src/ecs/components/position_component')
+
+local PlayerControlSystem = Concord.system({
+    pool = { "position" }
+})
+
+function PlayerControlSystem:update(dt)
+end
+
+return PlayerControlSystem
